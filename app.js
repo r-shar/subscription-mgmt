c0c4b247ee3c1c05ceac1df1bb86119c0bb21037
Rashmi Sharma <rashmisharma@Rashmis-MacBook-Air-2.local>
2025-04-07 18:54:31 -0700

    error handling

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ import authRouter from "./routes/auth.routes.js";
 import userRouter from "./routes/user.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import connectDB from "./config/database/mongodb.js";
+import errorMiddleware from "./middleware/error.middleware.js";
+import cookieParser from "cookie-parser";
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Subscription Tracker API");
@@ -18,5 +24,7 @@ app.listen(PORT, async () => {
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
+
+app.use(errorMiddleware);
 
 export default app;
