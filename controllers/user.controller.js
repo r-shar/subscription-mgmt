11fcb4e884a5f08316d378bb24e77a9bd1a2f58a
Rashmi Sharma <rashmisharma@Rashmis-MacBook-Air-2.local>
2025-04-10 16:38:17 -0700

    adding authorization for get user endpts

---
+++ controllers/user.controller.js
@@ -0,0 +1,43 @@
+import User from "../config/models/user.model.js";
+
+export const getUsers = async (req, res, next) => {
+    try {
+
+        const users = await User.find();
+
+        if (!users) {
+            const error = new Error("No users found");
+            error.statusCode = 404;
+            throw error;
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "Users fetched successfully",
+            data: users
+        })
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const getUser = async (req, res, next) => {
+    try {
+
+        const user = await User.findById(req.params.id).select("-hashedPassword"); // get all user info except pw info
+
+        if (!user) {
+            const error = new Error("No such user found");
+            error.statusCode = 404;
+            throw error;
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "User fetched successfully",
+            data: user
+        })
+    } catch (error) {
+        next(error);
+    }
+}
---
+++ middleware/auth.middleware.js
@@ -0,0 +1,36 @@
+import jwt from "jsonwebtoken";
+import User from "../config/models/user.model.js";
+import { JWT_SECRET } from "../config/env.js";
+
+const authorize = async (req, res, next) => {
+    try {
+
+        let token; 
+        if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+            token = req.headers.authorization.split(" ")[1];
+        }
+
+        if (!token) {
+            const error = new Error("No token provided");
+            error.statusCode = 401;
+            throw error;
+        }
+
+        const decoded = jwt.verify(token, JWT_SECRET);
+
+        req.user = await User.findById(decoded.userId);
+
+        next();
+        
+        
+    } catch (error) {
+        res.status(401).json({
+            success: false,
+            message: "Unauthorized",
+            error: error.message
+        });
+        next(error);
+    }
+}
+
+export default authorize;
